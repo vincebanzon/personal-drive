9930156c7c445ccb07287ead5bc8e86d56d1c1a9
Vince Banzon <vincebanzon@gmail.com>
2022-01-26 18:35:54 +0800

    Setup PORT and FOLDER environment

---
+++ index.js
@@ -0,0 +1,34 @@
+const process = require('process');
+
+
+console.log('hello world', process.env.PORT)
+console.log('hello world', process.env.FOLDER)
+
+/**
+ * TDD
+ * Install mocha and chai
+ *
+ * Update script:
+ *  npm start
+ *  npm test
+ *
+ * Setup EnvVar:
+ *  PORT
+ *  FOLDER
+ *
+ *
+ * Create a server
+ *  Create sample get endpoint
+ *  Test endpoint
+ *
+ * Create test for endpoints
+ * Create endpoints
+ *  POST /files
+ *  GET  /files/:publicKey
+ *  Delete /files/privateKey
+ *
+ * Read parameters
+ *
+ *
+ *
+ */
